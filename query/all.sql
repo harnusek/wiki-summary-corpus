01391064f060cb760029d01c92fd434bce5689c8
harnusek <ondrej.harnusek@gmail.com>
2018-09-22 13:16:18 +0200

    Updated query

--- query/all.sql
+++ query/all.sql
@@ -1,18 +1,33 @@
-delete from domain
-delete from summary
-delete from sentence
+-- delete from domain
+-- delete from summary
+-- delete from sentence
 
-select *
+select count(*)
 from domain
 
-select *
+select count(*)
 from summary
 
-select string_agg(text,' ')
+select count(*)
 from sentence
-group by summary_id
-order by rank
 
-select json_object_agg(rank,text)
+-- Get number of sentence by summary
+select summary_id, count(id) 
 from sentence
 group by summary_id
+order by summary_id
+
+-- Get avarage number of sentence by domain
+select domain_id, avg(count)
+from(select summary_id, count(id) 
+    from sentence
+    group by summary_id) as sub
+join summary on summary_id = summary.id
+group by domain_id
+order by domain_id
+
+-- Get aggreated text of summaries
+select summary_id, string_agg(text,' ' order by rank)
+from sentence
+group by summary_id
+limit 20
