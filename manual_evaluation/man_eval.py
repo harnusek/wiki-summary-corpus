737a7a687681cf578f8bbc8d5da7cdadda0b45a7
harnusek <ondrej.harnusek@gmail.com>
2019-04-25 13:41:59 +0200

    man eval

--- manual_evaluation/man_eval.py
+++ manual_evaluation/man_eval.py
@@ -4,13 +4,10 @@
 """
 Requires running server and outputs from experiment
 """
-
 import requests
 import json
-import io
-from random import shuffle
 import math
-EXPERIMENT_NAME = 'average'
+import os
 
 def all_config_testing():
     for method in ['corpusSim','knowledgeSim']:
@@ -21,8 +18,8 @@ def all_config_testing():
 
 def compare_method(method, use_lem, use_pos, use_stop):
     sum_difference = 0
-    number_experiments = len(experiment["rows"])
-    for row in experiment["rows"]:
+    number_experiments = len(EXPERIMENT)
+    for row in EXPERIMENT:
         sent_1 = row["sent_1"]
         sent_2 = row["sent_2"]
         man_sim = row["sim"]
@@ -30,7 +27,6 @@ def compare_method(method, use_lem, use_pos, use_stop):
         computed_sim = get_similarity(sent_1,sent_2,method, use_lem, use_pos, use_stop)
         diff = man_sim - computed_sim
         sum_difference = sum_difference + (diff*diff)
-        # print(man_sim, computed_sim, method + str(use_lem) + str(use_pos) + str(use_stop) + ' ' + sent_1 + ' ' + sent_2)
 
     avg_diff = sum_difference/(number_experiments-2)
     error = round(math.sqrt(avg_diff), 4)
@@ -50,17 +46,26 @@ def get_similarity(sent_1, sent_2, method, use_lem, use_pos, use_stop):
     response = requests.post(url, data=json.dumps(data), headers=headers)
     return float(response.content)
 
-def load_experiment(name):
-    with open('experiments/'+name+'.json', 'r', encoding="utf8") as json_file:
-        json_str = json_file.read()
-        return json.loads(json_str)
+def averaged_experiments():
+    names = os.listdir("experiments")
+    experiments = list()
+    for name in names:
+        experiments.append(load_experiment(name))
+    final = []
+    for exp in zip(*experiments):
+        sims = [line['sim'] for line in exp]
+        # averaging similarity of all experiments
+        avg_sim = (sum(sims)/len(sims))
+        print(sims, avg_sim/4)
+        final.append(({'sent_1':exp[0]['sent_1'], 'sent_2':exp[0]['sent_2'], 'sim':avg_sim}))
+    return final
 
-def generate_json_from_select_sentences(name):
+def load_experiment(name):
     rows = []
     sent_1, sent_2, sim = None, None, None
-    with open(name + '.txt', 'r', encoding="utf8") as fp:
-        line = fp.readline()
-        cnt = 0
+    with open('experiments/'+name, 'r', encoding='utf8') as file_in:
+        line = file_in.readline()
+        cnt, id = 0, 1
         while line:
             if(cnt%3 == 0):
                 sent_1 = line.strip()
@@ -68,20 +73,16 @@ def generate_json_from_select_sentences(name):
                 sent_2 = line.strip()
             if(cnt%3 == 2):
                 sim = line.strip()
-                rows.append({"sent_1":sent_1, "sent_2":sent_2, "sim":float(sim)})
-            line = fp.readline()
+                rows.append({"id":id, "sent_1":sent_1, "sent_2":sent_2, "sim":float(sim)})
+                id = id+1
+            line = file_in.readline()
             cnt += 1
-    shuffle(rows)
-    print('rows:',len(rows))
-    dictionary = {"rows":rows}
-    with io.open(name+'.json', 'w', encoding='utf8') as json_file:
-        json.dump(dictionary, json_file, ensure_ascii=False)
+    return rows
 
 if __name__ == '__main__':
-    # generate_json_from_select_sentences(name = EXPERIMENT_NAME)
-    experiment = load_experiment(EXPERIMENT_NAME)
-    fname = "reports/" + EXPERIMENT_NAME + "-comparation--e.txt"
-    file = open(fname, "w")
-    file.write('pos tagset basic, ' + EXPERIMENT_NAME + ' experiment comparation\n\n')
-    all_config_testing()
-    file.close()
+    EXPERIMENT = averaged_experiments()
+    # fname = "reports/report-averaged-new.txt"
+    # file = open(fname, "w")
+    # file.write('pos tagset basic, experiment comparation\n\n')
+    # all_config_testing()
+    # file.close()
