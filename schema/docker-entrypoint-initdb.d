a9322518d700921b19a7c0599bb23854a7fb2a10
Ondrej Harnusek <Ondrej.Harnusek@ibm.com>
2019-05-23 10:14:35 +0200

    Added docker stuff

---
+++ schema/docker-entrypoint-initdb.d
@@ -0,0 +1,20 @@
+CREATE TABLE domain
+(
+    id serial PRIMARY KEY,
+    label varchar(20) UNIQUE NOT NULL
+);
+CREATE TABLE summary
+(
+    id serial PRIMARY KEY,
+    title varchar(50) NOT NULL,
+    pageid integer UNIQUE NOT NULL,
+    url varchar(100) UNIQUE NOT NULL,
+    domain_id integer NOT NULL REFERENCES domain(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE sentence
+(
+    id serial PRIMARY KEY,
+    rank integer NOT NULL,
+    text text NOT NULL,
+    summary_id integer NOT NULL  REFERENCES summary(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
